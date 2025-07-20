1fd89b3c7508501c6565ebb196215d5a681c4902
Tango55555 <tangocar363@gmail.com>
2025-07-20 18:14:40 +0100

    Finished attendance taking using webcam program

--- Attendance_project.py
+++ Attendance_project.py
@@ -2,6 +2,7 @@ import face_recognition
 import cv2
 import numpy as np
 import os
+from datetime import datetime
 
 
 # Function that takes in a list of images and returns a list of encodings for the images
@@ -14,11 +15,25 @@ def find_encodings(image_list):
     return encode_list
 
 
+# Function that marks the name and time of detection in attendance.csv
+def mark_attendance(attendance_name):
+    with open('attendance.csv', 'r+') as f:
+        my_data_list = f.readlines()
+        name_list =[]
+        for line in my_data_list:
+            entry = line.split(',')
+            name_list.append(entry[0])
+        if attendance_name not in line:
+            now = datetime.now()
+            dt_string = now.strftime("%H:%M:%S")
+            f.writelines("\n" + attendance_name + ", " + dt_string)
+
+
 path = 'ImagesAttendance'
 images = []
 className = []
 myList = os.listdir(path)
-print("Total Classes Detected:",len(myList))
+print("Total Classes Detected:", len(myList))
 for x, cl in enumerate(myList):
     curImg = cv2.imread(f'{path}/{cl}')
     images.append(curImg)
@@ -26,11 +41,14 @@ for x, cl in enumerate(myList):
 
 encodeListKnown = find_encodings(images)
 print('Encodings Complete')
+print('Press q to quit webcam')
 
 cap = cv2.VideoCapture(0)
 
 while True:
     success, img = cap.read()
+    if not success:
+        break
 
     # Resize image to become smaller to make program run faster
     imgS = cv2.resize(img, (0, 0), fx=0.25, fy=0.25)
@@ -40,7 +58,7 @@ while True:
     facesCurFrame = face_recognition.face_locations(imgS)
     encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
 
-    # Match found face encodings to known list
+    # Match found face encodings on webcam to known list
     for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
         matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
         faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
@@ -48,15 +66,26 @@ while True:
         # Match is the one with the lowest distance
         matchIndex = np.argmin(faceDis)
 
-        if matches[matchIndex]:
+        # Labels name as unknown if face doesn't match any of the encodings
+        if faceDis[matchIndex] < 0.50:
             name = className[matchIndex].upper()
-            y1, x2, y2, x1 = faceLoc
+            mark_attendance(name)
+        else:
+            name = 'Unknown'
 
-            # Multiplied by 4 because image was scaled down prior
-            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
-            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
-            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_DUPLEX, 1.0, (255, 255, 255), 1)
+        # print(name)
+        y1, x2, y2, x1 = faceLoc
+        y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+        cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
 
     cv2.imshow('Webcam', img)
-    cv2.waitKey(1)
+
+    # Press 'q' to quit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release webcam and destroy all windows
+cap.release()
+cv2.destroyAllWindows()
